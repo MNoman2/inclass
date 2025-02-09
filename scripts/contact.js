b6915dacce0a6d99f8e97d19dd10109a224a4726
Muhammad Noman <mnoman2001@yahoo.com>
2025-02-09 18:10:47 -0500

    in class Webpack project

---
+++ scripts/contact.js
@@ -0,0 +1,13 @@
+import './../scss/contact.scss'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('contactForm');
+
+    if(form){
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            alert('success');
+        })
+    }
+
+})
